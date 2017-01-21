b477042f2a21b86b849b93c6b79e90e2dfd0ded5
Evoque <rubievoque@gmail.com>
2017-01-21 23:43:22 +0800

    first commit

--- websockets-server.js
+++ websockets-server.js
@@ -1,13 +1,13 @@
 
 /* handles all initialization and events related to the WebSocket */
 
-var WebSocket = require('ws');
+var WebSocket = require('ws')
 
 // working WebSockets server.
-var WebSocketServer = WebSocket.Server;
-var port = 3001;
+var WebSocketServer = WebSocket.Server
+var port = 3001
 var ws = new WebSocketServer({
   port: port
-});
+})
 
-console.log(`websockets server started at port ${port}`);
+console.log(`websockets server started at port ${port}`)
