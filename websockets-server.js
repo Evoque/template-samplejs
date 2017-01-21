fc136f487c541352931acdc0ae5c044e8ee9a0b1
Evoque <rubievoque@gmail.com>
2017-01-21 23:23:29 +0800

    Awesome Module

---
+++ websockets-server.js
@@ -0,0 +1,13 @@
+
+/* handles all initialization and events related to the WebSocket */
+
+var WebSocket = require('ws');
+
+// working WebSockets server.
+var WebSocketServer = WebSocket.Server;
+var port = 3001;
+var ws = new WebSocketServer({
+  port: port
+});
+
+console.log(`websockets server started at port ${port}`);
